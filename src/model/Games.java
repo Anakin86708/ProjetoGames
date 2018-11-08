d82d05c0e7c1142a3843f4e2c8a80ddf7bc2cf90
Anakin86708 <silva.ariel2013@gmail.com>
2018-11-08 14:43:14 -0200

    Formulário para inserir e editar, sem funcionamento ainda.

---
+++ src/model/Games.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ariel
+ */
+public class Games {
+    private String titulo, plataforma, idiomas, faixa_etaria, conteudo_embalagem, genero;
+    private int codigo, codigo_barras, codigo_marca, codigo_fornecedor;
+    private float preco, avaliacao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public String getIdiomas() {
+        return idiomas;
+    }
+
+    public void setIdiomas(String idiomas) {
+        this.idiomas = idiomas;
+    }
+
+    public String getFaixa_etaria() {
+        return faixa_etaria;
+    }
+
+    public void setFaixa_etaria(String faixa_etaria) {
+        this.faixa_etaria = faixa_etaria;
+    }
+
+    public String getConteudo_embalagem() {
+        return conteudo_embalagem;
+    }
+
+    public void setConteudo_embalagem(String conteudo_embalagem) {
+        this.conteudo_embalagem = conteudo_embalagem;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigo_barras() {
+        return codigo_barras;
+    }
+
+    public void setCodigo_barras(int codigo_barras) {
+        this.codigo_barras = codigo_barras;
+    }
+
+    public int getCodigo_marca() {
+        return codigo_marca;
+    }
+
+    public void setCodigo_marca(int codigo_marca) {
+        this.codigo_marca = codigo_marca;
+    }
+
+    public int getCodigo_fornecedor() {
+        return codigo_fornecedor;
+    }
+
+    public void setCodigo_fornecedor(int codigo_fornecedor) {
+        this.codigo_fornecedor = codigo_fornecedor;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public float getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(float avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+    
+    
+    
+}
