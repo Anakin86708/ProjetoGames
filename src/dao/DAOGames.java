88a6e63a6862cd7f785e0ee8de2d53834ab4af87
Anakin86708 <silva.ariel2013@gmail.com>
2018-11-17 11:53:17 -0200

    Inserir em marcas está funcionando corretamente. Ainda resta inserir em games  e fornecedores, além de editar e remover todos os registros.

--- src/dao/DAOGames.java
+++ src/dao/DAOGames.java
@@ -10,6 +10,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import model.Games;
 
 /**
  *
@@ -39,4 +40,34 @@ public class DAOGames {
         
         return resultSet;
     }
+    
+    public int nextIdGamesInt() {
+        try {
+            sqlString = "SHOW TABLE STATUS WHERE `Name` = 'games'";
+            statemant = connection.prepareStatement(sqlString);
+            resultSet = statemant.executeQuery(sqlString);
+            resultSet.first();
+            return (Integer) resultSet.getInt("Auto_increment");
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+    
+    public String insertMarca(Games games) {
+        try {
+            sqlString = "INSERT INTO games (codigo_barras, codigo_marca, titulo, plataforma, idiomas, faixa_etaria, conteudo_fornecedor, genero, codigo_fornecedor, preco, avaliacao) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+            ps = connection.prepareStatement(sqlString);
+            //
+            //PAREI AQUI
+            //Pegar informações do model
+            //
+            ps.setString(1, marca.getMarca());
+            ps.setString(2, marca.getCidade());
+            ps.setInt(3, marca.getTelefone());
+            ps.setInt(4, marca.getQtdGames());
+            ps.execute();
+            return "Sucesso!";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
 }
