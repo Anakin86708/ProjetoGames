4f5cd092ad2a4cf928da4da5adc48d81ed11de0b
Anakin86708 <silva.ariel2013@gmail.com>
2018-11-19 13:38:39 -0200

    Versão 1.0 Permite inserir, editar e remover registros de três tabelas: Games, Marcas e Fornecedores Os dados são exibidos em uma tabela

--- src/dao/DAOFornecedor.java
+++ src/dao/DAOFornecedor.java
@@ -5,11 +5,13 @@
  */
 package dao;
 
+import gui.form_Main;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import model.Fornecedor;
 
 /**
  *
@@ -40,6 +42,67 @@ public class DAOFornecedor {
         }
         
         return resultSet;
+    }
+    
+    public int nextIdFornecedorInt() {
+        try {
+            sqlString = "SHOW TABLE STATUS WHERE `Name` = 'fornecedor'";
+            statemant = connection.prepareStatement(sqlString);
+            resultSet = statemant.executeQuery(sqlString);
+            resultSet.first();
+            return (Integer) resultSet.getInt("Auto_increment");
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+    
+    public String insertFornecedor(Fornecedor fornecedor){
+        try {
+            sqlString = "INSERT INTO fornecedor (fornecedor, garantia_fornecedor, sac, cidade, ceo, qtdGames) VALUES (?,?,?,?,?,?)";
+            ps = connection.prepareStatement(sqlString);
+            
+            ps.setString(1, fornecedor.getFornecedor());
+            ps.setInt(2, fornecedor.getGarantia_fornecedor());
+            ps.setInt(3, fornecedor.getSac());
+            ps.setString(4, fornecedor.getCidade());
+            ps.setString(5, fornecedor.getCeo());
+            ps.setInt(6, fornecedor.getQtdGames());
+            ps.execute(); 
+            return "Sucesso!";
+        } catch (Exception e) {
+            return  e.getMessage();
+        }
+    }
+    
+    public String updateFornecedor(Fornecedor fornecedor){
+        try {
+            sqlString = "UPDATE fornecedor SET fornecedor = ?, garantia_fornecedor = ?, sac = ?, cidade = ?, ceo = ?, qtdGames = ? WHERE codigo_fornecedor = ?";
+            ps = connection.prepareStatement(sqlString);
+            
+            ps.setString(1, fornecedor.getFornecedor());
+            ps.setInt(2, fornecedor.getGarantia_fornecedor());
+            ps.setInt(3, fornecedor.getSac());
+            ps.setString(4, fornecedor.getCidade());
+            ps.setString(5, fornecedor.getCeo());
+            ps.setInt(6, fornecedor.getQtdGames());
+            ps.setInt(7, fornecedor.getCodigo_fornecedor());
+            ps.executeUpdate(); 
+            return "Sucesso!";
+        } catch (Exception e) {
+            return  e.getMessage();
+        }
+    }
+    
+    public String removeFornecedor(int id){
+        try {
+            sqlString = "DELETE FROM fornecedor WHERE codigo_fornecedor = ?";
+            ps = connection.prepareStatement(sqlString);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            return  "Sucesso!";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
     
 }
