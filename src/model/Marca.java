55d272cf3eb19986af01233d16e65ef9caf444f4
Anakin86708 <silva.ariel2013@gmail.com>
2018-11-19 13:38:35 -0200

    Versão 1.0 Permite inserir, editar e remover registros de três tabelas: Games, Marcas e Fornecedores Os dados são exibidos em uma tabela

--- src/model/Marca.java
+++ src/model/Marca.java
@@ -33,24 +33,36 @@ public class Marca {
         return codigo_marca;
     }
 
-    public void setCodigo_marca(int codigo_marca) {
-        this.codigo_marca = codigo_marca;
+    public void setCodigo_marca(String codigo_marca) {
+        try {
+            this.codigo_marca = Integer.parseInt(codigo_marca);
+        } catch (Exception e) {
+            this.codigo_marca = 0;
+        }
     }
 
     public int getTelefone() {
         return telefone;
     }
 
-    public void setTelefone(int telefone) {
-        this.telefone = telefone;
+    public void setTelefone(String telefone) {
+        try {
+            this.telefone = Integer.parseInt(telefone);
+        } catch (Exception e) {
+            this.telefone = 0;
+        }
     }
 
     public int getQtdGames() {
         return qtdGames;
     }
 
-    public void setQtdGames(int qtdGames) {
-        this.qtdGames = qtdGames;
+    public void setQtdGames(String qtdGames) {
+        try {
+            this.qtdGames = Integer.parseInt(qtdGames);
+        } catch (Exception e) {
+            this.qtdGames = 0;
+        }
     }
     
     
